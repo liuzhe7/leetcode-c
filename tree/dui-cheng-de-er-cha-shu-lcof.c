d5c2c02b657f7026c02ff1bcce59de1dc1f57d8e
liuzhe7 <zhixizhixizhixi@gmail.com>
2023-07-12 18:02:32 +0800

    tree

---
+++ tree/dui-cheng-de-er-cha-shu-lcof.c
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+typedef int bool;
+
+typedef struct TreeNode {
+     int val;
+      struct TreeNode *left;
+     struct TreeNode *right;
+ };
+
+bool isMirror(struct TreeNode* left,struct TreeNode* right) {
+  if (left == 0 && right != 0) {
+    return 0;
+  }
+    if (left != 0 && right== 0) {
+    return 0;
+  }
+
+  if (left == 0 && right == 0) {
+    return 1;
+  }
+
+  return isMirror(left->right,right->left) && isMirror(left->left,right->right) && right->val == left->val;
+
+}
+bool isSymmetric(struct TreeNode* root){
+  if (root==0) {
+    return 1;
+  }
+
+  return isMirror(root->left,root->right);
+}
+
---
+++ tree/maximum-depth-of-binary-tree.c
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+typedef struct TreeNode {
+     int val;
+     struct TreeNode *left;
+     struct TreeNode *right;
+};
+
+// f(x) = max(f(x.r),f(x.l)) + 1
+// return 1 x.r or x.l == nil
+int maxDepth(struct TreeNode* root){
+  if (root == 0){
+    return 0;
+  }
+
+  int maxLeft = maxDepth(root->left);
+  int maxRight = maxDepth(root->right);
+
+
+  return maxLeft > maxRight?maxLeft +1 :maxRight + 1;
+}
