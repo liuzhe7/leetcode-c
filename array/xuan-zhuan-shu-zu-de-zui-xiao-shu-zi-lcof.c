b76a75abae54e7cffe32c6c20c5155448c42ba24
liuzhe7 <zhixizhixizhixi@gmail.com>
2023-07-21 10:52:08 +0800

    arrar

---
+++ array/xuan-zhuan-shu-zu-de-zui-xiao-shu-zi-lcof.c
@@ -0,0 +1,17 @@
+int minArray(int* numbers, int numbersSize){
+  int min = 0;
+  int max = numbersSize -1;
+  int mid;
+  for (;max > min;)
+  {
+    mid = min + ((max - min) >> 1);
+    if ( numbers[mid] > numbers[max]) {
+      min = mid + 1;
+    } else if (numbers[mid] < numbers[max]) {
+      max = mid;
+    } else if (numbers[mid] == numbers[max]) {
+      max--;
+    }
+  }
+  return numbers[min];
+}
